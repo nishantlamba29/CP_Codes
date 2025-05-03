3304041fb7156670ba60eb8c117608821d77da25
Nishant Lamba <nishantlamba016@gmail.com>
2025-05-03 09:50:39 +0530

    pta nhi kyu

--- L_Controllers.cpp
+++ L_Controllers.cpp
@@ -3,8 +3,28 @@ using namespace std;
 #define int long long
 
 inline void n_lamba_29() {
-
-
+    int n;
+    string s;
+    cin >> n >> s;
+    int q;
+    cin >> q;
+    int x = count(s.begin(), s.end(), '+');
+    int y = n - x;
+    while(q--) {
+        int a, b;
+        cin >> a >> b;
+        int val = a*y - b*x;
+        if(a == b) {
+            if(x == y) cout << "YES\n";
+            else cout << "NO\n";
+        }
+        else if(val % (a-b) != 0) cout << "NO\n";
+        else {
+            int k = val / (a-b);
+            if(k >= 0 && k <= n) cout << "YES\n";
+            else cout << "NO\n";
+        }
+    }
 
 }
 
@@ -13,12 +33,7 @@ int32_t main(){
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
 
-    int _;
-    cin >> _;
-    while (_-->0) {
-        n_lamba_29();
-    }
-
+    n_lamba_29();
 
     return 0;
 }
